f8250379bb96e35c0c01791dbe02b4490c1b64d2
radu122T <radu.cudalb@yahoo.ro>
2022-07-12 18:34:06 +0300

    12.07.2022

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,9 +5,9 @@ const Post = require('../models/Post')
 
 
 router.post('/', async (req,res) => {
-    console.log(req.body)
+
     const post = new Post ( {
-        
+        cart: req.body.cart,
         shippingNume : req.body.dataShipping.values.nume,
         shippingPrenume : req.body.dataShipping.values.prenume,
         shippingEmail : req.body.dataShipping.values.email,
@@ -16,12 +16,12 @@ router.post('/', async (req,res) => {
         shippingLocalitate : req.body.dataShipping.values.localitate,
         shippingJudet : req.body.dataShipping.values.judet,
         shippingCod : req.body.dataShipping.values.codPostal,
-        paymentNume : req.body.dataPayment.values.nume,
-        paymentPrenume : req.body.dataPayment.values.prenume,
-        paymentAdresa : req.body.dataPayment.values.adresa,
-        paymentLocalitate:req.body.dataPayment.values.localitate,
-        paymentJudet:req.body.dataPayment.values.judet,
-        paymentCod:req.body.dataPayment.values.codPostal,
+        paymentNume : (req.body.dataPayment.length>0 && req.body.dataPayment.values.nume),
+        paymentPrenume : (req.body.dataPayment.length>0 && req.body.dataPayment.values.prenume),
+        paymentAdresa : (req.body.dataPayment.length>0 && req.body.dataPayment.values.adresa),
+        paymentLocalitate:(req.body.dataPayment.length>0 && req.body.dataPayment.values.localitate),
+        paymentJudet:(req.body.dataPayment.length>0 && req.body.dataPayment.values.judet),
+        paymentCod:(req.body.dataPayment.length>0 && req.body.dataPayment.values.codPostal)
     })
     
     try {
